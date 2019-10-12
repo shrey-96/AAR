cb1f126d212e763180dd5f57fbdee252d15737ca
shrey-96 <tiwarishreyansh15@rediffmail.com>
2019-10-12 02:23:31 -0400

    Spring security implemented for user sign up

--- mobile-app-ws/src/main/java/com/shrey/app/ws/service/UserService.java
+++ mobile-app-ws/src/main/java/com/shrey/app/ws/service/UserService.java
@@ -1,8 +1,10 @@
 package com.shrey.app.ws.service;
 
+import org.springframework.security.core.userdetails.UserDetailsService;
+
 import com.shrey.app.ws.shared.dto.UserDto;
 
-public interface UserService {
+public interface UserService extends UserDetailsService {
 
 	UserDto createUser(UserDto user);
 	
--- mobile-app-ws/src/main/java/com/shrey/app/ws/service/impl/UserServiceImpl.java
+++ mobile-app-ws/src/main/java/com/shrey/app/ws/service/impl/UserServiceImpl.java
@@ -2,6 +2,9 @@ package com.shrey.app.ws.service.impl;
 
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.shrey.app.ws.UserRepository;
@@ -19,6 +22,9 @@ public class UserServiceImpl implements UserService {
 	@Autowired
 	Utils utils;
 	
+	@Autowired
+	BCryptPasswordEncoder bCryptPasswordEncoder;
+	
 	
 	@Override
 	public UserDto createUser(UserDto user) {
@@ -30,7 +36,7 @@ public class UserServiceImpl implements UserService {
 		BeanUtils.copyProperties(user, userEntity);
 		
 		String publicUserId = utils.generateUserId(30);
-		userEntity.setEncryptedPassword("test");
+		userEntity.setEncryptedPassword(bCryptPasswordEncoder.encode(user.getPassword()));
 		userEntity.setUserId(publicUserId);
 		
 		
@@ -40,6 +46,13 @@ public class UserServiceImpl implements UserService {
 		BeanUtils.copyProperties(storedUserDetails, returnValue);
 		
 		return returnValue;
+	}
+
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 }
